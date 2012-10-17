6a71b1c6f0b8597064c3f17dae15107e7808dc62
dungvn3000@gmail.com <ducdung2512>
2012-10-17 15:39:20 +0700

    Add DataSet.

---
+++ src/main/scala/example1/Main.scala
@@ -0,0 +1,16 @@
+package example1
+
+import Recommendations._
+import DataSet._
+/**
+ * The Class Main.
+ *
+ * @author Nguyen Duc Dung
+ * @since 10/17/12 3:20 PM
+ *
+ */
+object Main extends App {
+
+  sim_pearson(critics, "Lisa Rose", "Gene Seymour")
+
+}
